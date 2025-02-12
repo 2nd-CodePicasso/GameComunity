318b56811d55391103cd0d64fda950fd33a7e243
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-13 00:54:38 +0900

    🎨 리퀘스트Dto에 밸리데이션 추가

--- src/main/java/com/example/codePicasso/domain/gameProposal/dto/request/ReviewGameProposalRequest.java
+++ src/main/java/com/example/codePicasso/domain/gameProposal/dto/request/ReviewGameProposalRequest.java
@@ -1,8 +1,10 @@
 package com.example.codePicasso.domain.gameProposal.dto.request;
 
 import com.example.codePicasso.domain.gameProposal.enums.ProposalStatus;
+import jakarta.validation.constraints.NotBlank;
 
 public record ReviewGameProposalRequest(
+        @NotBlank
         ProposalStatus status
 ) {
 }
--- src/main/java/com/example/codePicasso/domain/games/dto/request/UpdateGameRequest.java
+++ src/main/java/com/example/codePicasso/domain/games/dto/request/UpdateGameRequest.java
@@ -1,6 +1,9 @@
 package com.example.codePicasso.domain.games.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateGameRequest(
+        @NotBlank
         String gameDescription
 ) {
 }
