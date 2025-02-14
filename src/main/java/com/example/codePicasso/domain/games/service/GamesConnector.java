1aeb94d8ba93d2e3621fea985310839439aa8e0b
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-14 13:06:37 +0900

    🔥 뭐야잇 저리가 게임즈

--- src/main/java/com/example/codePicasso/domain/games/service/GamesConnector.java
+++
@@ -1,9 +0,0 @@
-package com.example.codePicasso.domain.games.service;
-
-import com.example.codePicasso.domain.games.entity.Games;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface GamesConnector {
-    Games findById(Long aLong);
-}
