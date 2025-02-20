68916c3a27937e38945c6d5df991fc500cd295b8
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-20 18:30:41 +0900

    🚑 1.0.10 핫픽스(포스트와 유저 연관관계 ManyToOne로 수정)

--- src/main/java/com/example/codePicasso/domain/post/entity/Post.java
+++ src/main/java/com/example/codePicasso/domain/post/entity/Post.java
@@ -18,7 +18,7 @@ public class Post extends TimeStamp {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
