9898f693ddbac1517792ee2498f1715bb11d0856
taejunUM <utj0411@naver.com>
2025-03-01 21:03:44 +0900

    ⚡️ : Post  QueryDSL 추가

--- src/main/java/com/example/codePicasso/domain/post/repository/PostConnectorImpl.java
+++ src/main/java/com/example/codePicasso/domain/post/repository/PostConnectorImpl.java
@@ -5,15 +5,26 @@ import com.example.codePicasso.domain.post.enums.PostStatus;
 import com.example.codePicasso.domain.post.service.PostConnector;
 import com.example.codePicasso.global.exception.base.InvalidRequestException;
 import com.example.codePicasso.global.exception.enums.ErrorCode;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.codePicasso.domain.category.entity.QCategory.category;
+import static com.example.codePicasso.domain.game.entity.QGame.game;
+import static com.example.codePicasso.domain.post.entity.QPost.post;
+import static com.example.codePicasso.domain.user.entity.QUser.user;
 
 @Component
 @RequiredArgsConstructor
 public class PostConnectorImpl implements PostConnector {
     private final PostRepository postRepository;
+    private final JPAQueryFactory queryFactory;
 
     // 게시글 생성
     @Override
@@ -24,26 +35,76 @@ public class PostConnectorImpl implements PostConnector {
     // gameId로 게시글 전체 조회
     @Override
     public Page<Post> findAllByGameId(Long gameId, Pageable pageable) {
-        return postRepository.findAllByGameId(gameId, pageable);
+        List<Post> posts = queryFactory.select(post)
+                .from(post)
+                .leftJoin(post.user, user).fetchJoin()
+                .leftJoin(post.category, category).fetchJoin()
+                .leftJoin(post.game, game).fetchJoin()
+                .where(post.game.id.eq(gameId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = Optional.ofNullable(queryFactory.select(post.count())
+                        .from(post)
+                        .fetchOne())
+                .orElse(0L);
+
+        return new PageImpl<>(posts, pageable, count);
     }
 
     // 게임별 추천게시물 조회
     @Override
     public Page<Post> findAllRecommendedOfGame(Long gameId, PostStatus postStatus, Pageable pageable) {
-        return postRepository.findAllByGameIdAndStatus(gameId, postStatus, pageable);
+        List<Post> posts = queryFactory.select(post)
+                .from(post)
+                .leftJoin(post.user, user).fetchJoin()
+                .leftJoin(post.category, category).fetchJoin()
+                .leftJoin(post.game, game).fetchJoin()
+                .where(post.game.id.eq(gameId), post.status.eq(postStatus))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = Optional.ofNullable(queryFactory.select(post.count())
+                        .from(post)
+                        .fetchOne())
+                .orElse(0L);
+
+        return new PageImpl<>(posts, pageable, count);
     }
 
     // categoryId로 게시글 전체 조회
     @Override
     public Page<Post> findAllByCategoryId(Long categoryId, Pageable pageable) {
-        return postRepository.findAllByCategoryId(categoryId, pageable);
+        List<Post> posts = queryFactory.select(post)
+                .from(post)
+                .leftJoin(post.user, user).fetchJoin()
+                .leftJoin(post.category, category).fetchJoin()
+                .leftJoin(post.game, game).fetchJoin()
+                .where(post.category.id.eq(categoryId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = Optional.ofNullable(queryFactory.select(post.count())
+                        .from(post)
+                        .fetchOne())
+                .orElse(0L);
+
+        return new PageImpl<>(posts, pageable, count);
     }
 
     // 게시글 개별 조회
     @Override
     public Post findById(Long postId) {
-        return postRepository.findById(postId)
-                .orElseThrow(() -> new InvalidRequestException(ErrorCode.POST_NOT_FOUND));
+        return queryFactory.select(post)
+                .from(post)
+                .leftJoin(post.user, user).fetchJoin()
+                .leftJoin(post.category, category).fetchJoin()
+                .leftJoin(post.game, game).fetchJoin()
+                .where(post.id.eq(postId))
+                .fetchOne();
     }
 
     // 게시글 수정
