24aab7d95fdc8d22d50a37adabbfcdd0acfb9463
HappyBeny <qkdehdgur3@gmail.com>
2025-03-10 09:07:05 +0900

    환경변수 사용 import 추가
    Signed-off-by: HappyBeny <qkdehdgur3@gmail.com>

--- src/main/java/com/example/codePicasso/global/config/RedisConfig.java
+++ src/main/java/com/example/codePicasso/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.example.codePicasso.domain.chat.service.RedisSubscriber;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
