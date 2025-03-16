33b49c7cd148f376050a7984a6ff375df0970ff1
afkorigin <gnstldgk@naver.com>
2025-03-16 11:48:19 +0900

    :sparkles: 카카오 회원가입 바들바들 기능 추가

--- src/main/java/com/example/codePicasso/domain/auth/controller/HomeController.java
+++ src/main/java/com/example/codePicasso/domain/auth/controller/HomeController.java
@@ -1,10 +1,26 @@
 package com.example.codePicasso.domain.auth.controller;
 
+import com.example.codePicasso.domain.auth.service.KaKaoService;
+import com.example.codePicasso.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final KaKaoService kaKaoService;
+
+    @GetMapping("/auth/hi/kakao/signup")
+    public String kakaoCallback(@RequestParam("code") String code, Model model) {
+        String token = kaKaoService.requestKakaoToken(code);
+        model.addAttribute("kakaoToken", token);
+
+        return "login_callback";
+    }
 
     @GetMapping("/login/hi/users")
     public String home() {
@@ -35,4 +51,5 @@ public class HomeController {
     public String gameBoard() {
         return "gameboard";
     }
+
 }
