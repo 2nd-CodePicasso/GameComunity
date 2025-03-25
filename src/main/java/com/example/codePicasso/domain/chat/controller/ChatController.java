2fb2a0e7f901d25d079409c59eb58e823256ad59
afkorigin <gnstldgk@naver.com>
2025-03-25 16:07:50 +0900

    :sparkles: 레빗mq 익명 큐 응용

--- src/main/java/com/example/codePicasso/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/codePicasso/domain/chat/controller/ChatController.java
@@ -6,6 +6,7 @@
     import com.example.codePicasso.domain.chat.dto.response.NotificationResponse;
     import com.example.codePicasso.domain.chat.service.ChatService;
     import com.example.codePicasso.domain.chat.service.NotificationService;
+    import com.example.codePicasso.domain.chat.service.RabbitPublisher;
     import com.example.codePicasso.domain.chat.service.RedisPublisher;
     import lombok.RequiredArgsConstructor;
     import lombok.extern.slf4j.Slf4j;
@@ -20,7 +21,7 @@
         private final ChatService chatService;
         private final NotificationService notificationService;
         private final RedisPublisher redisPublisher;
-       // private final RabbitPublisher rabbitPublisher;
+        private final RabbitPublisher rabbitPublisher;
 
         @MessageMapping("/send/all")
         public void sendMessage(
@@ -35,7 +36,7 @@
           //  redisPublisher.publishMessage(chatRequest,Long.valueOf(userId),username);
 
             //레빗MQ
-           // rabbitPublisher.publishMessage(chatRequest, Long.valueOf(userId), username);
+            rabbitPublisher.publishMessage(chatRequest, Long.valueOf(userId), username);
         }
 
         @MessageMapping("/send/room/{roomId}")
--- src/main/java/com/example/save_chat/rabbit/GlobalChatRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.example.save_chat.rabbit;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface GlobalChatRepository extends JpaRepository<GlobalChat,Long> {
-//}
